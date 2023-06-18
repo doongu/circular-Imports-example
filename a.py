9f39c084f519ea3e1d4009f638951abd1b304712
doongu <must1080@naver.com>
2023-06-19 00:52:05 +0900

    circular imports 파일 추가

---
+++ a.py
@@ -0,0 +1,7 @@
+import b
+
+def function_a():
+    print('function_a')
+    return b.function_b()
+
+function_a()
---
+++ b.py
@@ -0,0 +1,8 @@
+import a
+
+def function_b():
+    print('function_b')
+
+def function_c():
+    print('function_c')
+    return a.function_a()
